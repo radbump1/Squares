2fa2946019992b695ff164180ec82b9c226cb9e2
50616030 <50616030@50616030-PC>
2018-02-04 17:53:27 +0900

    50616030

--- src/squares/Squares_main.java
+++ src/squares/Squares_main.java
@@ -1,9 +1,12 @@
 package squares;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 
 public class Squares_main {
@@ -15,8 +18,9 @@ public class Squares_main {
 		double theta[] = new double[2];
 		Squares_main smain = new Squares_main();
 		double x[] = smain.getCSV1("csv/x.csv", 7);
-		double y[] = smain.getCSV1("csv/x.csv", 7);
+		double y[] = smain.getCSV1("csv/y.csv", 7);
 		double value[] = new double[100];
+		double value2[][] = new double[100][4];
 		
 		Squares_lib slib = new Squares_lib(x, y);
 		for(int i = 0; i < 100; i++){
@@ -25,10 +29,16 @@ public class Squares_main {
 			System.out.println(i+" : theta[1] = "+theta[1]);
 			System.out.println(i+" : Objective function = "+slib.getObject());
 			value[i] = slib.getObject();
+			value2[i][0] = i;
+			value2[i][1] = slib.getObject();
+			value2[i][2] = theta[0];
+			value2[i][3] = theta[1];
 		}
 		Graph graph = new Graph(value);
 	    graph.setBounds(5,5,655,455);
 	    graph.setVisible(true);
+	    
+	    smain.writeCSV("csv/result.csv",value2);
 
 	}
 
@@ -56,6 +66,31 @@ public class Squares_main {
 	    System.out.println("CSV取り込み = "+Arrays.toString(csvdata));
 	    return csvdata;
 	    //CSVから取り込みここまで
+	}
+	
+	public void writeCSV(String path, double[][] data) {		
+		try {
+		  FileWriter fw = new FileWriter(path, true);
+		  PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+		  //内容を指定する
+		  pw.println("id,objective,theta0,theta1");
+	      for(int i = 0; i < data.length; i++) {
+	    	  for(int j = 0; j < data[0].length; j++) {
+	    		  pw.print(data[i][j]);
+		          pw.print(","); 
+	    	  }    
+	    	  pw.println();
+	      }
+	      pw.println();
+	      //ファイルに書き出す
+	      pw.close();
+	        
+	      //終了メッセージを画面に出力する
+	      System.out.println("出力完了 : "+path);        
+		} catch (IOException e) {
+	      // TODO Auto-generated catch block
+		  e.printStackTrace();
+		}
 	}
 	
 }
